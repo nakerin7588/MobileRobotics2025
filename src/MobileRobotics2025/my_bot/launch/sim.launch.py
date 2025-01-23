043cd3358fdb560ab409ff717db091557c78e72f
SunnY <93282477+nakerin7588@users.noreply.github.com>
2025-01-23 10:42:11 +0700

    Add Classwork

---
+++ src/MobileRobotics2025/my_bot/launch/sim.launch.py
@@ -0,0 +1,110 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+import xacro
+
+def generate_launch_description():
+    package_name = "my_bot"
+    rviz_file_name = "gazebo.rviz"
+    rviz_file_path = os.path.join(
+        get_package_share_directory(package_name),
+        'rviz',
+        rviz_file_name
+    )
+
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name),
+                    "launch",
+                    "rsp.launch.py"
+                )
+            ]
+        ),
+        launch_arguments={"use_sim_time":"true"}.items()
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("gazebo_ros"),
+                    "launch",
+                    "gazebo.launch.py"
+                )
+            ]
+        )
+    )
+
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=[
+            "-topic", "robot_description",
+            "-entity", "my_bot"
+        ],
+        output = "screen"
+    )
+    
+    controller = Node(
+    	package="my_controller",
+    	executable="diff_drive.py"
+    )
+    
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        parameters=[{"use_sim_time": False}]
+    )
+
+    velocity_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["velocity_controllers", "--controller-manager", "/controller_manager"],
+        parameters=[{"use_sim_time": False}]
+    )
+
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=[
+            "-d", rviz_file_path
+        ],
+        output = "screen"
+    )
+
+    launch_description = LaunchDescription()
+
+    launch_description.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[joint_state_broadcaster_spawner],
+            )
+        )
+    )
+
+    launch_description.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[velocity_controller_spawner],
+            )
+        )
+    )
+
+    # Add the rest of the nodes and launch descriptions
+    launch_description.add_action(rviz)
+    launch_description.add_action(gazebo)
+    launch_description.add_action(spawn_entity)
+    launch_description.add_action(controller)
+    launch_description.add_action(rsp)
+    return launch_description
